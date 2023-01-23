b5dc09feb45928175dd1fccbc48d07b3d18b9e5e
IgorSapach <ihar.sapach@innowise-group.com>
2023-01-23 16:57:34 +0100

    Changed home page title

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,7 +2,7 @@
   <div
     class="translate-y-[-50%] translate-x-[-50%] top-1/2 left-1/2 absolute text-3xl font-bold underline"
   >
-    Now you in home page
+    Now you are on the home page
   </div>
 </template>
 
